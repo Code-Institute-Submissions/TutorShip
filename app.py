46f76ea30705c5a1fba38cb5c845b28483a5743c
Justin McCarthy <carthymcjustin@gmail.com>
2020-09-09 17:12:23 +0100

    Add app.py and connect database
    Update gitignore, base.html. Add requirements.txt and tutors.html

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+  import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'tutorshipDB'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_subjects')
+def get_tasks():
+    return render_template("tutors.html", subjects=mongo.db.subjects.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=(os.environ.get('PORT')),
+    debug=True)
