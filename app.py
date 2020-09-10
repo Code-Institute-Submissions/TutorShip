d94b43980fdc4b9892752927b58ac91061753651
Justin McCarthy <carthymcjustin@gmail.com>
2020-09-10 12:50:14 +0100

    Add register.html template and app route.
    Add register button link in base.html

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def tutors():
 def login():
     return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=(os.environ.get('PORT')),
