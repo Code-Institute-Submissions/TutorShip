b0e1359e270dabf5aa5c5a870b70447f37a57afa
Justin McCarthy <carthymcjustin@gmail.com>
2020-09-25 17:02:05 +0100

    Fix JS errors revealed by JShint

--- static/js/form_validation.js
+++ static/js/form_validation.js
@@ -1,35 +1,35 @@
-const fname = document.getElementById('first_name')
-const lname = document.getElementById('last_name')
-const about = document.getElementById('about')
-const qualification = document.getElementById('qualification')
-const img = document.getElementById('profile_image')
-const form = document.getElementById('form')
-const errorText = document.getElementById('error')
+const fname = document.getElementById('first_name');
+const lname = document.getElementById('last_name');
+const about = document.getElementById('about');
+const qualification = document.getElementById('qualification');
+const img = document.getElementById('profile_image');
+const form = document.getElementById('form');
+const errorText = document.getElementById('error');
 
 form.addEventListener('submit', (e) => {
-  let errorMessage = []
+  let errorMessage = [];
   if (fname.value.trim() == "") {
-    errorMessage.push('Please enter valid first name.')
+    errorMessage.push('Please enter valid first name.');
   }
   
   if (lname.value.trim() == "") {
-    errorMessage.push('Please enter valid last name.')
+    errorMessage.push('Please enter valid last name.');
   }
   
   if (about.value.trim() == "") {
-    errorMessage.push('Please enter some information about yourself.')
+    errorMessage.push('Please enter some information about yourself.');
   }
   
   if (qualification.value.trim() == "") {
-    errorMessage.push('Please enter a qualification.')
+    errorMessage.push('Please enter a qualification.');
   }
   
   if (img.value.trim() == "") {
-    errorMessage.push('Please enter a valid image link.')
+    errorMessage.push('Please enter a valid image link.');
   }
 
   if (errorMessage.length > 0) {
-    e.preventDefault()
-    errorText.innerText = errorMessage.join(' ')
+    e.preventDefault();
+    errorText.innerText = errorMessage.join(' ');
   }
-})
+});
